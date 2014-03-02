18e072e8900f36e14e8349d40b08ea88a74d3594
Nadav Sinai <ns@nadavsinai.com>
2014-03-02 16:23:50 +0200

    add sort menu

--- studio/featuresStructure.php
+++ studio/featuresStructure.php
@@ -1,22 +1,22 @@
 <?php
 return array(
-	"basicDisplay"=> array(
-		"label" => "Basic Display",
-		"type" => "menu",
-		"description" => "Basic settings let you set player name, entry and aspect ratio.",
-		"model" => "basicDisplay",
-		"icon" => "TabBasicSettings",
-		"children"=> array(
-			array(
-				"label" => "Player's Name",
-				"type" => "text",
-				"player-refresh"=> false,
-				"require"=> true,
-				"model" => "name",
-				"default" => "New Player",
-				"helpnote" => "Please enter your player's name",
-				"endline" => "true"
-			),
+    "basicDisplay" => array(
+        "label" => "Basic Display",
+        "type" => "menu",
+        "description" => "Basic settings let you set player name, entry and aspect ratio.",
+        "model" => "basicDisplay",
+        "icon" => "TabBasicSettings",
+        "children" => array(
+            array(
+                "label" => "Player's Name",
+                "type" => "text",
+                "player-refresh" => false,
+                "require" => true,
+                "model" => "name",
+                "default" => "New Player",
+                "helpnote" => "Please enter your player's name",
+                "endline" => "true"
+            ),
 //			array(
 //				"label" => "Player Tags",
 //				"type" => "tags",
@@ -24,114 +24,130 @@ return array(
 //				"model" => "tags",
 //				"source" => "getTags"
 //			),
-			array(
-				"label" => "Preview entry",
-				"type" => "select2data",
-				"source" => "listEntries",
-				"query" => "queryEntries",
-				"helpnote" => "Select entry",
-				"player-refresh"=> true,
-				"endline" => "true",
-				"width" => "100%",
-				"model" => "~settings.previewEntry",
-				"data-placeholder" => "Pick an entry"
-			),
-			array(
-				"player-refresh" => "aspectToggle",
-				"options"=> array(
-					array(
-						"label" => "4/3",
-						"value" => "narrow"
-					),
-					array(
-						"label" => "16/9",
-						"value" => "wide"
-					)
-				),
-				"showSearch"=> false,
-				"initvalue" => "wide",
-				"helpnote" => "Select aspect ratio",
-				"type" => "dropdown",
-				"label" => "Aspect Ratio",
-				"endline" => "true",
-				"model" => "basicDisplay.aspectRatio"
-			),
-			array(
-				"label" => "Automatically play video on page load",
-				"type" => "checkbox",
-				"endline" => "true",
-				"model" => "config.uiVars.autoPlay"
-			),
-			array(
-				"label" => "Start player muted",
-				"type" => "checkbox",
-				"endline" => "true",
-				"model" => "config.uiVars.autoMute"
-			),
-			array(
-				"label" => "Last Update",
-				"type" => "readonly",
-				"filter" => "timeago",
-				"model" => "updatedAt"
-			)
-		)
-	),
-	"lookAndFeel"=> array(
-		"label" => "Look and Feel",
-		"icon" => "TabLookandFeel",
-		"description" => "Adjust the visual appearance of the player.",
-		"type" => "menu",  
-		"model" => "lookAndFeel",
-		"children"=> array(
-			"titleLabel" => "",
-			"share" => "",
-			"closedCaptions" => "",
-			"volumeControl" => "",
-			"keyboardShortcuts" => "",
-			"watermark" => "",
-			"moderation" => "",
-			"theme" => ""
-		)
-	),
-	"analytics"=> array(
-		"label" => "Analytics",
-		"icon" => "TabAnalytics",
-		"description" => "Kaltura supports robust analytics via the Kaltura platform as well as via 3rd party analytics providers.",
-		"type" => "menu",
-		"model" => "analytics",
-		"children"=> array(
-			"akamaiMediaAnalytics" => "",
-			"googleAnalytics" => "",
-			"comscore" => "",
-			"nielsenCombined" => "",
-			"omnitureOnPage" => "",
-			"statistics" => "",
-		)
-	),
-	"monetization"=> array(
-		"label" => "Monetization",
-		"icon" => "TabMonetization",
-		"description" => "The Kaltura platform supports VAST 3.0 as well as 3rd party ad plugins to facilitate content monetization.",
-		"type" => "menu",
-		"model" => "monitization",
-		"children"=> array(
-			"bumper" => "",
-			"vast" => "",
-			"doubleClick" => "",
-			"freeWheel" => "",
-			"tremor" => ""
-		)
-	),
-	"plugins"=> array(
-		"label" => "Plugins",
-		"icon" => "TabPlugins",
-		"description" => "Additional plugins",
-		"type" => "menu",
-		"model" => "plugins",
-		"children"=> array(
-			/*"chaptersView" => "",*/
-			"playbackRateSelector" => "",
-			"restrictUserAgent" => ""
-		)
-	)
+            array(
+                "label" => "Preview entry",
+                "type" => "select2data",
+                "allow-custom-values" => "Not from your user's entries",
+                "source" => "listEntries",
+                "query" => "queryEntries",
+                "helpnote" => "Select entry",
+                "player-refresh" => true,
+                "endline" => "true",
+                "width" => "100%",
+                "model" => "~settings.previewEntry",
+                "data-placeholder" => "Pick an entry"
+            ),
+            array(
+                "player-refresh" => "aspectToggle",
+                "options" => array(
+                    array(
+                        "label" => "4/3",
+                        "value" => "narrow"
+                    ),
+                    array(
+                        "label" => "16/9",
+                        "value" => "wide"
+                    )
+                ),
+                "showSearch" => false,
+                "initvalue" => "wide",
+                "helpnote" => "Select aspect ratio",
+                "type" => "dropdown",
+                "label" => "Aspect Ratio",
+                "endline" => "true",
+                "model" => "basicDisplay.aspectRatio"
+            ),
+            array(
+                "label" => "Automatically play video on page load",
+                "type" => "checkbox",
+                "endline" => "true",
+                "model" => "config.uiVars.autoPlay"
+            ),
+            array(
+                "label" => "Start player muted",
+                "type" => "checkbox",
+                "endline" => "true",
+                "model" => "config.uiVars.autoMute"
+            ),
+            array(
+                "label" => "Last Update",
+                "type" => "readonly",
+                "filter" => "timeago",
+                "model" => "updatedAt"
+            )
+        )
+    ),
+    "lookAndFeel" => array(
+        "label" => "Look and Feel",
+        "icon" => "TabLookandFeel",
+        "description" => "Adjust the visual appearance of the player.",
+        "type" => "menu",
+        "model" => "lookAndFeel",
+        "children" => array(
+            "titleLabel" => "",
+            "share" => "",
+            "closedCaptions" => "",
+            "volumeControl" => "",
+            "keyboardShortcuts" => "",
+            "watermark" => "",
+            "moderation" => "",
+            "theme" => ""
+        )
+    ),
+    "analytics" => array(
+        "label" => "Analytics",
+        "icon" => "TabAnalytics",
+        "description" => "Kaltura supports robust analytics via the Kaltura platform as well as via 3rd party analytics providers.",
+        "type" => "menu",
+        "model" => "analytics",
+        "children" => array(
+            "akamaiMediaAnalytics" => "",
+            "googleAnalytics" => "",
+            "comscore" => "",
+            "nielsenCombined" => "",
+            "omnitureOnPage" => "",
+            "statistics" => "",
+        )
+    ),
+    "monetization" => array(
+        "label" => "Monetization",
+        "icon" => "TabMonetization",
+        "description" => "The Kaltura platform supports VAST 3.0 as well as 3rd party ad plugins to facilitate content monetization.",
+        "type" => "menu",
+        "model" => "monitization",
+        "children" => array(
+            "bumper" => "",
+            "vast" => "",
+            "doubleClick" => "",
+            "freeWheel" => "",
+            "tremor" => ""
+        )
+    ),
+    "plugins" => array(
+        "label" => "Plugins",
+        "icon" => "TabPlugins",
+        "description" => "Additional plugins",
+        "type" => "menu",
+        "model" => "plugins",
+        "children" => array(
+            /*"chaptersView" => "",*/
+            "playbackRateSelector" => "",
+            "restrictUserAgent" => ""
+        )
+    ),
+    "sort" => array(
+        "label" => "Sort Order",
+        "icon" => "TabEngagement",
+        "description" => "allows to sort plugins UI",
+        "type" => "menu",
+        "model" => "sortOrder",
+        "children" => array(
+            array(
+                "type" => "sortOrder",
+                "label" => "Sort Order",
+                "model" => "sortOrder.sortOrder"
+            )
+
+        )
+    )
 );
