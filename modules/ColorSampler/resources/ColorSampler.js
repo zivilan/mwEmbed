c68fb07154b0abbbd17ef2513ea4762eb4076864
amirch1 <amir.chervinsky@kaltura.com>
2015-12-27 14:35:04 +0200

    color sampler initial commit

---
+++ modules/ColorSampler/resources/ColorSampler.js
@@ -0,0 +1,108 @@
+( function( mw, $ ) {"use strict";
+
+
+	mw.PluginManager.add( 'colorSampler', mw.KBaseComponent.extend({
+
+		defaultConfig: {
+			'parent': 'controlsContainer',
+			'order': 57,
+			'visible': true,
+			'align': "right",
+			'showTooltip': true,
+			'tooltip': gM( 'mwe-ColorSampler-tooltip' ),
+			'sampleInterval': 500,
+			'position': 'bottom'
+		},
+		colorThief: null,
+		intervalID: null,
+		vid: null,
+		active: false,
+		$colorSamples: null,
+
+		setup: function( embedPlayer ) {
+			this.colorThief = new ColorThief();
+			this.addBindings();
+		},
+
+		addBindings: function() {
+			var _this = this;
+			$( this.embedPlayer).bind('onplay', function(){
+				if (!_this.intervalID && _this.active){
+					_this.startSample();
+				}
+			});
+
+			$( this.embedPlayer).bind('onpause', function(){
+				_this.stopSample();
+			});
+
+			$( this.embedPlayer).bind('playerReady', function(){
+				_this.vid = _this.embedPlayer.getPlayerElement();
+				$(_this.vid).attr("crossorigin", "anonymous");
+			});
+		},
+
+		startSample: function(){
+			var _this = this;
+			this.intervalID = setInterval(function(){
+				var frameColors = _this.colorThief.getPalette(_this.vid,3);
+				if (frameColors){
+					var color1 = frameColors[0];
+					var color2 = frameColors[1];
+					var color3 = frameColors[2];
+					var hex1 = mw.util.rgbToHex(color1[0],color1[1],color1[2]);
+					var hex2 = mw.util.rgbToHex(color2[0],color2[1],color2[2]);
+					var hex3 = mw.util.rgbToHex(color3[0],color3[1],color3[2]);
+//					_this.$colorSamples.find(".color1").html("<span style='font-size: 14px'>"+ntc.name(hex1)[1]+" </span>("+hex1+")").css("background-color","rgb("+color1+")");
+//					_this.$colorSamples.find(".color2").html("<span style='font-size: 14px'>"+ntc.name(hex2)[1]+" </span>("+hex2+")").css("background-color","rgb("+color2+")");
+//					_this.$colorSamples.find(".color3").html("<span style='font-size: 14px'>"+ntc.name(hex3)[1]+" </span>("+hex3+")").css("background-color","rgb("+color3+")");
+					_this.$colorSamples.find(".color1").html("<span style='font-size: 14px'>"+ntc.name(hex1)[1]+" </span>").css("background-color","rgb("+color1+")");
+					_this.$colorSamples.find(".color2").html("<span style='font-size: 14px'>"+ntc.name(hex2)[1]+" </span>").css("background-color","rgb("+color2+")");
+					_this.$colorSamples.find(".color3").html("<span style='font-size: 14px'>"+ntc.name(hex3)[1]+" </span>").css("background-color","rgb("+color3+")");
+				}
+			},this.getConfig("sampleInterval"));
+		},
+
+		stopSample: function(){
+			if (this.intervalID){
+				clearInterval(this.intervalID);
+				this.intervalID = null;
+			}
+		},
+
+		drawUI: function(){
+			this.$colorSamples = $("<div class='colorSamples'/>")
+				.addClass(this.getConfig("position"))
+				.append("<div class='colorSample color1'/>")
+				.append("<div class='colorSample color2'/>")
+				.append("<div class='colorSample color3'/>");
+			if (this.active){
+				colorSamples.addClass("active");
+			}
+			this.embedPlayer.getVideoHolder().append(this.$colorSamples);
+		},
+
+		getComponent: function() {
+			var _this = this;
+			if( !this.$el ) {
+				this.$el = $( '<button/>' )
+					.attr( 'title', gM( 'mwe-ColorSampler-tooltip' ) )
+					.addClass( "btn icon-eyedropper" + this.getCssClass() )
+					.click( function() {
+						_this.active = !_this.active;
+						var iconColor = _this.active ? "LawnGreen" : "white";
+						_this.embedPlayer.getInterface().find(".icon-eyedropper").css("color", iconColor);
+						if (_this.active){
+							_this.$colorSamples.addClass("active");
+							_this.startSample();
+						}else{
+							_this.$colorSamples.removeClass("active");
+							_this.stopSample();
+						}
+					});
+				_this.drawUI();
+			}
+			return this.$el;
+		}
+	}));
+} )( window.mw, window.jQuery );
