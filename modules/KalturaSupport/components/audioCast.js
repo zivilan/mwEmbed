04a2a71c57e44b5838b1e4e1a7f88964c338c664
Eliza Sapir <eliza.sapir@kaltura.com>
2015-12-28 18:14:20 +0200

    attaching guid + dynamic params

--- modules/KalturaSupport/components/audioCast.js
+++ modules/KalturaSupport/components/audioCast.js
@@ -18,7 +18,7 @@
 
             return false;
         },
-        addBindings: function() {debugger;
+        addBindings: function() {
             var _this = this;
 
             this.bind('playerReady', function() {
@@ -28,10 +28,11 @@
 
             this.bind('playing', function(){
                 setInterval(function(){
-                        _this.currentTime = _this.embedPlayer.currentTime;
-                        _this.createQRCode();
-                        _this.removeAudioCastButton();
-                        _this.addAudioCastButton();
+                        //_this.currentTime = _this.embedPlayer.currentTime;
+                        //_this.createQRCode();
+                        //_this.removeAudioCastButton();
+                        //_this.addAudioCastButton();
+                        _this.socket.emit('publish', _this.qId + ":currentTime=" + _this.embedPlayer.currentTime);
                     }
                     ,5000);
             });
@@ -46,15 +47,22 @@
 
             return this.$el;
         },
+        s4: function () {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        },
+        guid: function() {
+            return this.s4() + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' +
+                this.s4() + '-' + this.s4() + this.s4() + this.s4();
+        },
+        qId: 0,
         createQRCode: function(){
-            var ks = "NTA5OWE0NTgyZDBkMmYwMmFmODZhZjAzYzBhNTNlZDI3Y2NhODY1M3wxMDI7MTAyOzIzMTUyMjY0Mzg7MjsxNDUxMjI2NDM4Ljk4Nzg7eW9zc2kucGFwaWFzaHZpbGlAa2FsdHVyYS5jb207Ozs==";
-            var url = "http://www.kaltura.com";
+            this.qId = this.guid();
             var qrParams = {
                 entryId:this.embedPlayer.kentryid,
                 partnerId:this.embedPlayer.kpartnerid,
-                ks:ks,
-                currentTime:this.currentTime,
-                url:url
+                qId:this.qId
             };
 
             $("#qrcode").qrcode(JSON.stringify(qrParams));
@@ -67,10 +75,10 @@
         removeAudioCastButton: function(){
             $("#qrcode").remove();
         },
+        socket:null,
         go: function () {
-            var y = "p=" + this.embedPlayer.kpartnerid +"&x=q3";
-            var socket = io.connect("http://dev-backend15.dev.kaltura.com:3000/?"+y);
-            socket.emit('publish', "q3:hhhhh");
+            var y = "p=" + this.embedPlayer.kpartnerid +"&x="+ this.qId;
+            this.socket = io.connect("http://dev-backend15.dev.kaltura.com:3000/?"+y);
             return false;
         }
     }));
